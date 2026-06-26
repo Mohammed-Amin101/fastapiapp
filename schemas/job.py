395f246de19dbfd56e00bfbd9f6dfdf685a51668
mohd amin <sheikmohdamin@gmail.com>
2026-06-26 14:05:58 +0530

    update

---
+++ schemas/job.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel,Optional
+class JobCreate(BaseModel):
+    title:str
+    salary:int
+
+class JobUpdate(BaseModel):
+    title: Optional[str] = None
+    salary: Optional[int] = None
