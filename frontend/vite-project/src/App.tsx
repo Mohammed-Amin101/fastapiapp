ca3dddff8041905d862b632bb62bdec83f4724a3
mohd amin <sheikmohdamin@gmail.com>
2026-06-29 16:26:32 +0530

    update frontend

---
+++ frontend/vite-project/src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from './components/NavBar';
+import Welcome from './components/Welcome';
+function App(){
+  return (
+    <>
+      <Navbar />
+      <Welcome />
+    </>
+  )
+}
+export default App;
---
+++ frontend/vite-project/src/components/Welcome.tsx
@@ -0,0 +1,6 @@
+function Welcome() {
+    return  (
+        <h1>Welcome to My App</h1>
+    )
+}
+export default Welcome;
